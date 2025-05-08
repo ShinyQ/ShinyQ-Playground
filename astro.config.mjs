06a2c6e4ae633e1943d9d396ab45a2eb1f185dcf
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2025-05-09 03:50:28 +0700

    fix: remove astro node unused package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import path from "path";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
