849ea78a94915ff3b9645cbc1bc03a45f6f763dc
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2025-05-12 01:59:23 +0700

    fix: alias error on deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,8 @@ export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve('./src')
+        '@': path.resolve('./src'),
+        'react-dom/server': 'react-dom/server.edge'
       }
     },
   },
