ba29d1d31b5bd4d51a2b400833e7168e368e9119
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2025-05-12 01:42:10 +0700

    chore: Inject env using Vite's way

--- src/config.ts
+++ src/config.ts
@@ -9,13 +9,13 @@ function required(name: string): string {
 
 export const config = {
     GITHUB_API_TOKEN: required('GITHUB_API_TOKEN'),
-    REDIS_URL: process.env.REDIS_URL ?? 'redis://localhost:6379',
-    CACHE_KEY_TOP_REPO: process.env.CACHE_KEY_TOP_REPO ?? 'github:top_repos',
-    CACHE_TOP_REPO_TTL: parseInt(process.env.CACHE_TOP_REPO_TTL ?? '604800', 10),
+    REDIS_URL: import.meta.env.REDIS_URL ?? 'redis://localhost:6379',
+    CACHE_KEY_TOP_REPO: import.meta.env.CACHE_KEY_TOP_REPO ?? 'github:top_repos',
+    CACHE_TOP_REPO_TTL: parseInt(import.meta.env.CACHE_TOP_REPO_TTL ?? '604800', 10),
     CLOUDFLARE: {
-        ACCOUNT_ID: process.env.CLOUDFLARE_R2_ACCOUNT_ID,
-        ACCESS_KEY_ID: process.env.CLOUDFLARE_R2_ACCESS_KEY_ID,
-        SECRET_ACCESS_KEY: process.env.CLOUDFLARE_R2_SECRET_ACCESS_KEY,
-        BUCKET_NAME: process.env.CLOUDFLARE_R2_BUCKET_NAME
+        ACCOUNT_ID: import.meta.env.CLOUDFLARE_R2_ACCOUNT_ID,
+        ACCESS_KEY_ID: import.meta.env.CLOUDFLARE_R2_ACCESS_KEY_ID,
+        SECRET_ACCESS_KEY: import.meta.env.CLOUDFLARE_R2_SECRET_ACCESS_KEY,
+        BUCKET_NAME: import.meta.env.CLOUDFLARE_R2_BUCKET_NAME
     },
 } as const;
