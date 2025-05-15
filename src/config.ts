a48b4a9f2c69c167b54f9f33e8583395863b4717
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2025-05-15 12:31:48 +0700

    fix: using import meta for config

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-function required(name: string): string {
-    const value = process.env[name];
-    if (!value) throw new Error(`[ENV] Missing required env: ${name}`);
-    return value;
-}
-
 export const config = {
-    GITHUB_API_TOKEN: required('GITHUB_API_TOKEN'),
-    REDIS_URL: required('REDIS_URL'),
+    GITHUB_API_TOKEN: import.meta.env.GITHUB_API_TOKEN,
+    REDIS_URL: import.meta.env.REDIS_URL,
     CLOUDFLARE: {
-        ACCOUNT_ID: required('CLOUDFLARE_R2_ACCOUNT_ID'),
-        ACCESS_KEY_ID: required('CLOUDFLARE_R2_ACCESS_KEY_ID'),
-        SECRET_ACCESS_KEY: required('CLOUDFLARE_R2_SECRET_ACCESS_KEY'),
-        BUCKET_NAME: required('CLOUDFLARE_R2_BUCKET_NAME'),
+        ACCOUNT_ID: import.meta.env.CLOUDFLARE_R2_ACCOUNT_ID,
+        ACCESS_KEY_ID: import.meta.env.CLOUDFLARE_R2_ACCESS_KEY_ID,
+        SECRET_ACCESS_KEY: import.meta.env.CLOUDFLARE_R2_SECRET_ACCESS_KEY,
+        BUCKET_NAME: import.meta.env.CLOUDFLARE_R2_BUCKET_NAME,
     },
 } as const;
 
